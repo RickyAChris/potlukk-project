05abbe2f187a14e6fd3c07b6a369852f25c9912f
Adam <adam.ranieri@revature.com>
2023-02-10 15:20:45 -0500

    added basic routing for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { HomePage } from './pages/home-page';
+import { PotlukkDetailsGuestPage } from './pages/potlukk-details-guest-page';
+import { PotlukkDetailsHostPage } from './pages/potlukk-details-host-page';
+import { RegistrationPage } from './pages/registration-page';
+import { SignInPage } from './pages/sign-in-page';
 
 function App() {
   return <>
-  
-  
+    <BrowserRouter>
+
+    <Routes>
+      <Route path='/' element={<SignInPage/>}/>
+      <Route path='/home' element={<HomePage/>}/>
+      <Route path='/registration' element={<RegistrationPage/>}/>
+      <Route path='/potlukkinfohost/:potlukkID' element={<PotlukkDetailsHostPage/>}/>
+      <Route path='/potlukkinfoguest/:potlukkID' element={<PotlukkDetailsGuestPage/>}/>
+    </Routes>
+
+
+    </BrowserRouter>
   
   </>
 }
---
+++ src/pages/sign-in-page.tsx
@@ -0,0 +1,8 @@
+
+
+export function SignInPage(){
+
+    return <>
+        <h1>Sign-in Page</h1>
+    </>
+}
