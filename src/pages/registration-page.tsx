ce9b71830200c69ec7c6076738c25429d381999c
RickyAChris <richard590@revature.net>
2023-02-13 11:51:54 -0600

    return html start

--- src/pages/registration-page.tsx
+++ src/pages/registration-page.tsx
@@ -3,5 +3,21 @@
 export function RegistrationPage(){
     return <>
         <h1>Registration Page</h1>
+
+    <label>Create Username</label>
+    <input type="text" placeholder="johndoe"></input>
+
+    <label>Create Password</label>
+    <input type="text" placeholder="*******"></input>
+
+    <label>Confirm Password</label>
+    <input type="text" placeholder="*******"></input>
+
+    <h3>Allergens</h3>
+    
+    <input type="checkbox" id="milk"/><label htmlFor="milk">Milk</label>
+    <input type="checkbox" id="egg"></input><label htmlFor="egg">Egg</label>
+    <input type="checkbox" id="soy"></input><label htmlFor="soy">Soy</label>
+    <input type="checkbox" id="treenuts"></input><label htmlFor="treenuts">Tree Nuts</label>
     </>
 }
