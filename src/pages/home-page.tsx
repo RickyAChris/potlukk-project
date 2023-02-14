3cf6b3f3df10ee7dcc0197e26c217ad97fc7dca3
RickyAChris <richard590@revature.net>
2023-02-14 16:47:56 -0600

    html base for home page

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,7 +1,48 @@
+import { NavBar } from "../components/navbar";
 
 export function HomePage(){
 
+
     return <>
         <h1>Home Page</h1>
+        <NavBar/>
+
+       <table>
+        <thead>
+        <tr><th>Invited Potlukks</th></tr>
+        </thead>
+        <tbody>
+        <tr><td>test</td></tr>
+        <tr><td>test</td></tr>
+        <tr><td>test</td></tr>
+       </tbody>
+       </table>
+            
+       <table>
+        <thead>
+        <tr><th>Attending Potlukks</th></tr>
+        </thead>
+        <tbody>
+        <tr><td>test</td></tr>
+        <tr><td>test</td></tr>
+        <tr><td>test</td></tr>
+       </tbody>
+       </table>
+
+        <table>
+        <thead>
+        <tr><th>Notifications</th></tr>
+        </thead>
+        <tbody>
+        <tr><td>test</td></tr>
+        <tr><td>test</td></tr>
+        <tr><td>test</td></tr>
+       </tbody>
+       </table>    
+            
+    
+
+        
+       
     </>
 }
