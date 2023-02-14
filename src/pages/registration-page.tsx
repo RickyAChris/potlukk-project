381dcfe9e91c2e07151b43e3040d4e4c71fdef99
jpkeegan <82060340+jpkeegan@users.noreply.github.com>
2023-02-13 21:50:06 -0500

    Registration Page Main Features
    Added checkbox functionality and form validation for addUser function, test not working

--- src/pages/registration-page.tsx
+++ src/pages/registration-page.tsx
@@ -1,40 +1,86 @@
 import React, { useState } from "react"
-import { User, createUser, getAllUsers } from "../api/sign-in-requests"
+import { useMutation, useQueryClient } from "react-query"
+import { User, createUser, getAllUsers, UserCreation } from "../api/sign-in-requests"
 
 
 
 type UserForm = {
     username: string,
     password: string,
+    confirmPassword: string,
     fname: string,
     lname: string,
     allergies: string[]
-
 }
 
-type UserCreatorProps = {
-    setUser: React.Dispatch<React.SetStateAction<User[]>>
-}
 
-export function RegistrationPage(props:UserCreatorProps){
-    const [form,setForm] = useState<UserForm>({username:"", password:"", fname:"",lname:"",allergies:[]})
+export function RegistrationPage(){
+    const [form,setForm] = useState<UserForm>({
+        username:"",
+         password:"", 
+         confirmPassword:"",
+         fname:"",
+         lname:"",
+         allergies:[]})
 
-    async function submitData(){
-        const lukker = await createUser({
-            username: form.username,
-            password:form.password,
-            fname:form.fname,
-            lname:form.lname,
-            allergies: form.allergies
-        })
-        const users = await getAllUsers();
-        props.setUser(users);
+    const queryClient = useQueryClient();
+
+    const createUserMutation = useMutation(createUser, {
+        onSuccess: () => queryClient.invalidateQueries("usercache")
+    });
+
+    function isFormValid(){
+        if(!form.username || !form.password || !form.confirmPassword || !form.fname || !form.lname) {
+            alert("Please fill in all the fields");
+            return;
+        }
+
+        if(form.password.length < 10 || !/[!@#$%^&*(),.?":{}|<>]/.test(form.password)){
+            alert("Password must: Be atleast 10 characters long and Contain 1 special character");
+            return;
+        }
+        if(form.password !== form.confirmPassword){
+            alert("Passwords must match");
+            return;
+        }
+        return true;
     }
 
 
-    function checkBox(event:React.FormEvent){
-        console.log(event.target);
-        
+
+    function addUser(){
+        if(!isFormValid()){
+            return;
+        }
+        const newUser: UserCreation ={
+            username: form.username,
+            password: form.password,
+            fname: form.fname,
+            lname: form.lname,
+            allergies: form.allergies
+        }
+        createUserMutation.mutate(newUser);
+        alert("Account Successfully Created!");
+        window.location.href = "/";
+    }
+
+
+    function checkBoxHandler(event:React.FormEvent<HTMLInputElement>){
+        const checked = event.currentTarget.checked;
+        const id = event.currentTarget.id;
+
+        setForm(prevForm =>{
+            let newAllergies = [...prevForm.allergies];
+            if(checked){
+                newAllergies.push(id);
+            } else {
+                newAllergies = newAllergies.filter(allergy => allergy !== id);
+            }
+            return{
+                ...prevForm,
+                allergies: newAllergies
+            };
+        }); 
     }
 
 
@@ -42,28 +88,28 @@ export function RegistrationPage(props:UserCreatorProps){
         <h1>Registration Page</h1>
 
     <label>Create Username</label>
-    <input type="text" placeholder="johndoe123" onChange={e=>setForm({...form, username:e.target.value})}></input>
+    <input type="text" placeholder="janedoe123" onChange={e=>setForm({...form, username:e.target.value})}></input>
 
     <label>Create Password</label>
-    <input type="text" placeholder="*******" onChange={e=>setForm({...form, password:e.target.value})}></input>
+    <input type="text" placeholder="abc1234567!" onChange={e=>setForm({...form, password:e.target.value})}></input>
 
     <label>Confirm Password</label>
-    <input type="text" placeholder="*******" onChange={e=>setForm({...form, password:e.target.value})}></input>
+    <input type="text" placeholder="retype" onChange={e=>setForm({...form, confirmPassword:e.target.value})}></input>
 
     <label>First Name</label>
-    <input type="text" placeholder="John" onChange={e=>setForm({...form, fname:e.target.value})}></input>
+    <input type="text" placeholder="Jane" onChange={e=>setForm({...form, fname:e.target.value})}></input>
 
     <label>Last Name</label>
     <input type="text" placeholder="Doe" onChange={e=>setForm({...form, lname:e.target.value})}></input>
 
     <h3>Allergens</h3>
     
-    <input type="checkbox" id="MILK" onChange={checkBox}/><label htmlFor="MILK">Milk</label>
-    <input type="checkbox" id="EGG" onChange={checkBox}/><label htmlFor="EGG">Egg</label>
-    <input type="checkbox" id="SOY" onChange={checkBox}/><label htmlFor="SOY">Soy</label>
-    <input type="checkbox" id="TREENUTS" onChange={checkBox}/><label htmlFor="TREENUTS">Tree Nuts</label>
+    <input type="checkbox" id="MILK" onChange={checkBoxHandler}/><label htmlFor="MILK">Milk</label>
+    <input type="checkbox" id="EGG" onChange={checkBoxHandler}/><label htmlFor="EGG">Egg</label>
+    <input type="checkbox" id="SOY" onChange={checkBoxHandler}/><label htmlFor="SOY">Soy</label>
+    <input type="checkbox" id="TREENUTS" onChange={checkBoxHandler}/><label htmlFor="TREENUTS">Tree Nuts</label>
 
-    <button onClick={submitData}>Submit</button>
+    <button onClick={addUser}>Submit</button>
 
     </>
 }
