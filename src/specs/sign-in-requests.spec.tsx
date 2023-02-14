bb1f356a074e015365796947a5c2ba6ee4bac83a
jpkeegan <82060340+jpkeegan@users.noreply.github.com>
2023-02-14 12:14:11 -0500

    Updated the test for registration
    Updated test for registration, added queryclientprovider and expectation

--- src/specs/sign-in-requests.spec.tsx
+++ src/specs/sign-in-requests.spec.tsx
@@ -1,7 +1,9 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { createUser, getAllUsers, UserCreation } from "../api/sign-in-requests"
 import { RegistrationPage } from "../pages/registration-page";
+
 
 
 test("Get All Users", async() =>{
@@ -23,39 +25,33 @@ test("User Creation", async()=>{
     console.log(user);
 });
 
+const queryClient = new QueryClient();
 test("Should create a new user from input fields", async()=>{
 
-    render(<RegistrationPage/>);
+    
+
+    render(<QueryClientProvider client = {queryClient}>
+            <RegistrationPage/>
+        </QueryClientProvider>);
+    
 
     const usernameInput = await screen.findByPlaceholderText("janedoe123");
     const passwordInput = await screen.findByPlaceholderText("abc1234567!");
     const confirmPasswordInput = await screen.findByPlaceholderText("retype");
-    const fnameInput = await screen.getByLabelText('First Name');
-    const lnameInput = await screen.getByLabelText('Last Name');
+    const fnameInput =  await screen.findByPlaceholderText("Jane");
+    const lnameInput = await screen.findByPlaceholderText("Doe");
     const allergyCheck = await screen.findByLabelText("Milk");
-    const addUserButton = await screen.findByText("Submit")
+    const addUserButton = await screen.findByText("Submit");
+
 
     userEvent.type(usernameInput, "janedoe123");
     userEvent.type(passwordInput, "abc1234567!");
     userEvent.type(confirmPasswordInput, "abc1234567!");
     userEvent.type(fnameInput, "Jane");
-    userEvent.type(lnameInput, " Doe"); 
+    userEvent.type(lnameInput, "Doe"); 
     fireEvent.click(allergyCheck, true);
     userEvent.click(addUserButton);
+    
+    expect(screen.findByText(/Account Successfully Created!/));
 
-    await waitFor(() => {
-        expect(createUser).toHaveBeenCalledWith({
-          username: 'janedoe123',
-          password: 'abc1234567!',
-          fname: 'Jane',
-          lname: 'Doe',
-          allergies: ["MILK"],
-        });
-    })
-
-    expect(window.alert).toHaveBeenCalledWith('Account Successfully Created!');
-
-    await waitFor(() => {
-        expect(window.location.href).toBe("/");
-    });
-})
+});
