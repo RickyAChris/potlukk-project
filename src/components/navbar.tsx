7c71c902a4106518aa61a3861dd91a5d362014cb
RickyAChris <richard590@revature.net>
2023-02-15 08:49:14 -0600

    logout clear local and signup to reg

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,13 +3,16 @@ import { Link } from "react-router-dom";
 
 export function NavBar(){
 
+    function ClearLocalStorage() {
+        localStorage.clear();
+    }
 
     return <>
         <div className="NavBar">
 
             <Link to='/home'>Home</Link>
             <Link to='/host'>Host</Link>
-            <Link to='/'>Logout</Link>
+            <Link to='/' onClick={ClearLocalStorage}>Logout</Link>
 
         </div>
 
