144d52164e509dee4fefc7455299aa43e114ba65
RickyAChris <richard590@revature.net>
2023-02-16 14:09:14 -0600

    home update, does not function

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,6 +1,36 @@
+import { getPotlukks } from "../api/home-requests";
 import { NavBar } from "../components/navbar";
+import { useQuery } from "react-query"
 
 export function HomePage(){
+
+// User Features
+
+// Three main lists are available for view
+// Hosted Potlukks
+// Shows potlukks the lukker is hosting
+// Clicking on one forwards the user to a potlukk details page for the potlukk
+
+const {isLoading, data =[]} = useQuery("usercache", () => getPotlukks(Number(localStorage.getItem("userId"))));
+if(isLoading){
+    return<h1>LOADING</h1>
+}
+
+
+
+// Invited Potlukks          
+// Shows potlukks the lukker was invited too
+// Clicking on one forwards the user to a potlukk details page for the potlukk
+
+
+// Notifications
+// Shows all notifications that are related to the person
+// Events created by them
+// Events that affect a potlukk they host or attend
+
+// Technical Details
+// Navbar uses react router
+// clicking logout clears local storage and redirects to signin
 
 
     return <>
@@ -23,7 +53,7 @@ export function HomePage(){
         <tr><th>Attending Potlukks</th></tr>
         </thead>
         <tbody>
-        <tr><td>test</td></tr>
+        <tr>{data.map(p=> <td>{p.host.userId} {p.details.title}</td>)}</tr>
         <tr><td>test</td></tr>
         <tr><td>test</td></tr>
        </tbody>
